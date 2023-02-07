003b156727c3cf1ed461ec4c6e46bef0e8bc0f27
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-02-07 10:28:48 -0600

    my first commit

---
+++ src/main/java/misael_cuadrado/Main.java
@@ -0,0 +1,11 @@
+package misael_cuadrado;
+
+public class Main {
+    public static void main(String[] args) {
+        // the program starts executing here
+        System.out.println("Hello world!");
+    }
+}
+
+
+
